fd40e0ae2d4ba663e6ae3cd03693031a8d04d051
Unknown <santiagorugnitz@gmail.com>
2019-10-24 11:08:57 -0300

    atributos de tabla

--- src/Tabla.java
+++ src/Tabla.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+
 public class Tabla {
     Programa p1;
     Programa p2;
+    Variable[][] estados;
+    ArrayList<Variable> variables;
 
 
     public Tabla(Programa p1, Programa p2) {
