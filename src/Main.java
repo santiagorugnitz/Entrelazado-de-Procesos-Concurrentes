1ae277f50ba022c4de5e2815ba04001c6f25cb66
Santiago Rugnitz <santiagorugnitz@gmail.com>
2019-10-25 13:24:14 -0300

    antes de cambiar el main

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,9 @@ public class Main {
         System.out.println(f1.getAbsolutePath());
         Compilador c1 = new Compilador(f1,"Programa 1");
         Compilador c2 = new Compilador(f2,"Programa 2");
-        Compilador cMain = new Compilador(main,"Main");
-        if(c1.compilar()&&c2.compilar()&&cMain.compilar()){
-            Tabla t= new Tabla(c1.getPrograma(),c2.getPrograma(),cMain.getPrograma());
+        int[] valoresIniciales;//TODO Martin va a hacer eso
+        if(c1.compilar()&&c2.compilar()){
+            Tabla t= new Tabla(c1.getPrograma(),c2.getPrograma(),valoresIniciales);
             //t.mostrar????
         }
     }
--- src/Tabla.java
+++ src/Tabla.java
@@ -3,8 +3,10 @@ import java.util.ArrayList;
 public class Tabla {
     class Estado{
         int[] variables;
-        Estado anterior;
 
+        Estado(){
+            this.variables=new int[6];
+        }
     }
 
     Programa p1;
@@ -15,11 +17,17 @@ public class Tabla {
     int actual2=0;
 
 
-    public Tabla(Programa p1, Programa p2,Programa main) {
+    public Tabla(Programa p1, Programa p2,int valoresIni[]) {
         estados= new Estado[p1.largo][p2.largo];
+        for (int i = 0; i < p1.largo; i++) {
+            for (int j = 0; j < p2.largo; j++) {
+                estados[i][j]=new Estado();
+            }
+        }
         for (int i = 0; i <vUsadas.length ; i++) {
             vUsadas[i]=p1.variables[i]||p2.variables[i];
         }
+        estados[0][0]= new Estado;
         
     }
 }
