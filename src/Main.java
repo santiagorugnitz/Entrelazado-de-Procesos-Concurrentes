b7d71f3c243126b1ffdea887212c2c12c5eb348e
Martin <martin.gut@hotmail.com>
2019-10-25 13:39:51 -0300

    Cambios en main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
@@ -10,7 +12,34 @@ public class Main {
         System.out.println(f1.getAbsolutePath());
         Compilador c1 = new Compilador(f1,"Programa 1");
         Compilador c2 = new Compilador(f2,"Programa 2");
-        int[] valoresIniciales;//TODO Martin va a hacer eso
+        int[] valoresIniciales = new int[6];
+        ArrayList<String> operacionesConocidas = new ArrayList<String>();
+        Scanner in = new Scanner(main);
+        int i = 0;
+        while(in.hasNext() && i < 4){
+            String s = in.nextLine();
+            s = s.trim();
+            valoresIniciales[i] = Integer.parseInt(s.charAt(2)+"");
+            i++;
+        }
+
+        // 1 es True y 0 es False
+        while(in.hasNext() && i < 6){
+            String s = in.nextLine();
+            s = s.trim();
+            if (s.charAt(2) == 't'){
+                valoresIniciales[i] = 1;
+            }else{
+                valoresIniciales[i] = 0;
+            }
+            i++;
+        }
+
+        while(in.hasNext()){
+            String[] s = in.nextLine().split(" ");
+            operacionesConocidas.add(s[1]);
+        }
+
         if(c1.compilar()&&c2.compilar()){
             Tabla t= new Tabla(c1.getPrograma(),c2.getPrograma(),valoresIniciales);
             //t.mostrar????
